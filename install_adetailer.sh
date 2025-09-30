5b6ff14205dbfa3f0199a38208764e49eeff052d
4tyzap <123526259+4tyzap@users.noreply.github.com>
2025-09-30 02:04:03 -0700

    Create install_adetailer.sh

---
+++ install_adetailer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script installs the ADetailer extension for RuinedFooocus.
+# It clones the ADetailer repository into the 'adetailer' directory of this project.
+
+git clone https://github.com/Bing-su/adetailer.git adetailer
+
+echo "ADetailer extension installed in adetailer directory."
